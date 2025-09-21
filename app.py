0e6887dcd98581640050b861a30a99435c341e17
Богданов Семён <semerka.semen@yandex.ru>
2025-09-21 12:11:09 +0700

    Стили

--- app.py
+++ app.py
@@ -37,13 +37,17 @@ def author():
 
 @app.route('/image')
 def image():
-    path = url_for("static", filename="dub.jpg")
-    return '''
+    img_path = url_for("static", filename="dub.jpg")
+    css_path = url_for("static", filename="lab1.css")
+    return f'''
 <!doctype html>
 <html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="{css_path}">
+    </head>
     <body>
         <h1>Дуб</h1>
-        <img src="''' + path + '''">
+        <img src="{img_path}" alt="Дуб">
     </body>
 </html>
 '''
