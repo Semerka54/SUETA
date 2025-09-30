e06e7cbd8173d7fd984bc9a6f59ffc8f16f91a64
Богданов Семён <semerka.semen@yandex.ru>
2025-09-30 17:26:25 +0700

    Наследование шаблонов

--- app.py
+++ app.py
@@ -437,3 +437,7 @@ def example():
         {'name': 'манго', 'price': 321}
     ]
     return render_template('example.html', name = name, numer = numer, group = group, cours = cours, fruits = fruits)
+
+@app.route('/lab2/')
+def lab2():
+    return render_template('lab2.html')
