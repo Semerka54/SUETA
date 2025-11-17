b249442a982bc89b0f8ec81a309ac1bbfc38bfaa
Богданов Семён <semerka.semen@yandex.ru>
2025-11-18 01:28:31 +0700

    Аутентификация

--- lab5.py
+++ lab5.py
@@ -2,16 +2,52 @@ from flask import Blueprint, url_for, request, redirect, Response, render_templa
 from functools import wraps
 import datetime
 import psycopg2
+from psycopg2.extras import RealDictCursor
 
 lab5 = Blueprint('lab5', __name__)
 
 @lab5.route("/lab5/")
 def lab():
-    return render_template("lab5/lab5.html", username="Anonymous")
+    return render_template('lab5/lab5.html', login=session.get('login'))
 
-@lab5.route('/lab5/login')
+@lab5.route('/lab5/login', methods=['GET', 'POST'])
 def login():
-    return "Страница входа"
+    if request.method == 'GET':
+        return render_template('lab5/login.html')
+    
+    login = request.form.get('login')
+    password = request.form.get('password')
+    
+    if not login or not password:
+        return render_template('lab5/login.html', error="Заполните все поля")
+
+# Подключение к базе данных
+    conn = psycopg2.connect(
+        host='127.0.0.1',
+        database='saymon_bogdanov_knowledge_base',
+        user='saymon_bogdanov_knowledge_base',
+        password='123'
+    )
+    cur = conn.cursor(cursor_factory = RealDictCursor)
+
+    # Проверка существования пользователя (исправлен SQL-запрос)
+    cur.execute("SELECT * FROM users WHERE login=%s;", (login,))
+    user = cur.fetchone()
+
+    if not user:
+        cur.close()
+        conn.close()
+        return render_template('lab5/login.html', error='Логин и/или пароль неверны')
+
+    if user['password'] != password:
+        cur.close()
+        conn.close()
+        return render_template('lab5/login.html', error='Логин и/или пароль неверны')
+    
+    session['login'] = login
+    cur.close()
+    conn.close()
+    return render_template('lab5/success_login.html', login=login)
 
 @lab5.route('/lab5/register', methods=['GET', 'POST'])
 def register():
@@ -35,7 +71,8 @@ def register():
     cur = conn.cursor()
 
     # Проверка существования пользователя (исправлен SQL-запрос)
-    cur.execute("SELECT login FROM users WHERE login=%s;", (login,))
+    cur.execute("SELECT * FROM users WHERE login=%s;", (login,))
+
     if cur.fetchone():
         cur.close()
         conn.close()
