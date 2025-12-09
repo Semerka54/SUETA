26213f2ee4d1835b9ef83dc967daf098fa80be13
Богданов Семён <semerka.semen@yandex.ru>
2025-12-09 13:54:59 +0700

    Получение информации о фильме/-ах по REST

--- lab7.py
+++ lab7.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, render_template, session, g
+from flask import Blueprint, request, render_template, session, g, abort
 import sqlite3
 
 lab7 = Blueprint('lab7', __name__)
@@ -6,3 +6,49 @@ lab7 = Blueprint('lab7', __name__)
 @lab7.route('/lab7/')
 def lab():
     return render_template('lab7/lab7.html')
+
+
+films = [
+  {
+    "title": "Inception",
+    "title_ru": "Начало",
+    "year": 2010,
+    "description": "Научно-фантастический триллер о воре, который проникает в сны, чтобы украсть или внедрить идею."
+  },
+  {
+    "title": "The Matrix",
+    "title_ru": "Матрица",
+    "year": 1999,
+    "description": "Хакер узнаёт, что реальность — это симуляция, созданная искусственным интеллектом."
+  },
+  {
+    "title": "Interstellar",
+    "title_ru": "Интерстеллар",
+    "year": 2014,
+    "description": "Команда исследователей путешествует через червоточину, чтобы найти новую обитаемую планету."
+  },
+  {
+    "title": "The Shawshank Redemption",
+    "title_ru": "Побег из Шоушенка",
+    "year": 1994,
+    "description": "История несправедливо осуждённого банкира, который не теряет надежды на свободу."
+  },
+  {
+    "title": "The Lord of the Rings: The Fellowship of the Ring",
+    "title_ru": "Властелин колец: Братство кольца",
+    "year": 2001,
+    "description": "Хоббит Фродо отправляется в опасное путешествие, чтобы уничтожить могущественное Кольцо Всевластья."
+  }
+]
+
+
+@lab7.route('/lab7/rest-api/films/', methods=['GET'])
+def get_films():
+    return films
+
+
+@lab7.route('/lab7/rest-api/films/<int:id>', methods=['GET'])
+def get_film(id):
+    if id < 0 or id >= len(films):
+        abort(404)
+    return films[id]
